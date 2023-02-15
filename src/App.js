21aaac0bab9bc3a06a86708856545ea469389daf
Simas <simonas.aruna@gmail.com>
2023-02-15 09:18:00 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import UploadEmployees from "./components/UploadEmployees";
 import NotFound from "./components/NotFound";
 import Upload from "./components/Upload";
+import Layout from "./pages/Layout";
 import {BrowserRouter as Router, Routes, Route}
 from "react-router-dom";
 
 function App(){
   return (
+    <>
+    {/* <Layout/> */}
     <Router>
       <Routes>
         <Route exact path="/" element={<UploadEmployees/>} /> 
@@ -13,6 +16,8 @@ function App(){
         <Route path="/upload" element={<Upload/>}/>
       </Routes>
     </Router>
+    </>
+
   )
 }
 
---
+++ src/pages/Layout.js
@@ -0,0 +1,37 @@
+import {
+  Outlet,
+  Link,
+  NavLink,
+  useMatch,
+  useResolvedPath,
+} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <nav className="nav">
+      <CustomLink to="/"> 
+      <h1>Project</h1>
+      </CustomLink>
+
+      <ul>
+        <CustomLink to="/upload">Prekės </CustomLink>
+        <CustomLink to="/">Employees </CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  // const path = window.location.pathname
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
