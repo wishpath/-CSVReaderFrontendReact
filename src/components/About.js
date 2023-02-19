717c7978ee2b0dc758191fba477f3c6af7938a53
Simas <simonas.aruna@gmail.com>
2023-02-19 11:13:44 +0200

    Ready for deployment

--- src/components/About.js
+++ src/components/About.js
@@ -19,12 +19,20 @@ document.title = 'About';
             <img src="https://avatars.githubusercontent.com/u/117854313?v=4" className="photo"/>
             <br/>
             <a>
+              This is a homework task for Sourcery Academy / Devbridge.
+            </a>
+            <a href="https://github.com/wishpath/CSVReaderBackendJavaH2">Source code - backend</a>
+            <a href="https://github.com/wishpath/-CSVReaderFrontendReact">Source code - frontend</a>
+            <br/>
+            <a>
               Made by Simonas Aruna, soon to be known as a best programmer ever:
             </a>
             <a href="https://www.linkedin.com/in/simonas-aruna/">LinkedIn</a>
             <a href="https://github.com/wishpath">GitHub</a>
+            
+
             <a>+37063644328</a>
-            <a>simonasa.aruna[at]gmail.com</a>
+            <a>simonas.aruna[at]gmail.com</a>
             <br/>
           </div>   
         </Paper>
